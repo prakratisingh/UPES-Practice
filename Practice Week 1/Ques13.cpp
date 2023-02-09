760db81118a14ef32c2c04f0dfdc9122783f6e02
PRAKRATI SINGH <89121458+prakratisingh@users.noreply.github.com>
2023-02-09 22:58:44 +0530

    Pattern

---
+++ Practice Week 1/Ques13.cpp
@@ -0,0 +1,24 @@
+/*Write a program in C to make such a pattern like right angle triangle with number 
+increased by 1. 
+The pattern like : 
+ 1 
+ 2 3 
+ 4 5 6 
+ 7 8 9 10 
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int k = 1;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<i;j++)
+        {
+            cout<<k++<<" ";
+        }
+        cout<<endl;
+    }
+}
