df527f991118215737007b531392cfe597422fb1
PRAKRATI SINGH <89121458+prakratisingh@users.noreply.github.com>
2023-02-20 19:31:01 +0530

    input_string

---
+++ Practice Week 1/Ques37.cpp
@@ -0,0 +1,9 @@
+//Write a program in C to input a string and print it. 
+#include<iostream>
+using namespace std;
+int main()
+{
+    // string str;
+    // cin>>str;
+    // cout<<"The string: "<<str<<endl;
+}
